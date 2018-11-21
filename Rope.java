09121d01b4b933aa3f691bb2ecc40f026b39a864
Mehmet Yildiz <302574757@student.rocmondriaan.nl>
2018-11-21 14:23:14 +0100

    hij kan nu een touw klimmen

---
+++ Rope.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rope here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rope extends Tile
+{
+    /**
+     * Act - do whatever the Rope wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Rope(String image, int width, int heigth) { 
+     super(image,width,heigth); 
+      }
+}
